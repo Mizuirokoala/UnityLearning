f9e1bfc1652e87136faf0005b18ed45b955496fb
Damian Kamiński <Mizuirokoala@gmail.com>
2022-02-15 09:17:36 +0100

    Rotate and PickUp Method

--- New Unity ProjectBall/Assets/Scripts/PlayerController.cs
+++ New Unity ProjectBall/Assets/Scripts/PlayerController.cs
@@ -6,9 +6,7 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 0;
-
     private Rigidbody rb;
-
     private float movementX;
     private float movementY;
 
@@ -21,7 +19,6 @@ public class PlayerController : MonoBehaviour
     private void OnMove(InputValue movementValue)
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
-
         movementX = movementVector.x;
         movementY = movementVector.y;
     }
@@ -29,8 +26,13 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
-
         rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        { other.gameObject.SetActive(false); }
     }
 
 }
---
+++ New Unity ProjectBall/Assets/Scripts/RotateController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateController : MonoBehaviour
+{
+    public float speed;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+    
+        
+            
+
+    }
+
+
+}
